b2b7fb11a0191a5378ecd5a3138291690bcfc316
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2024-05-06 01:14:50 +0530

    reading params.

--- src/app/task/task-detail/task-detail.component.ts
+++ src/app/task/task-detail/task-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Task } from '../task.model';
+import { ActivatedRoute } from '@angular/router';
+import * as TasksSource from "../../../assets/tasks.json";
 
 @Component({
   selector: 'app-task-detail',
@@ -9,6 +11,13 @@ import { Task } from '../task.model';
 export class TaskDetailComponent {
 
   task!: Task;
-  constructor(){
+  constructor(private route: ActivatedRoute){
+    
+  }
+
+  ngOnInit(){
+    const id = this.route.snapshot.paramMap.get('id');
+    console.log(id);
+    this.task = TasksSource.tasks.find(i=> i.id==Number(id)) as Task;
   }
 } 

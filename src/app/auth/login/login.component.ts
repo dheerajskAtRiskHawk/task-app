8b432d85d0879f694fac9c9b8df698298ad55225
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2024-05-06 01:58:39 +0530

    navigate programmatically

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +9,22 @@ import { Component } from '@angular/core';
 export class LoginComponent {
   username: string = '';
   password: string = '';
+  isShowError?:boolean = false;
+
+  constructor(private router: Router){}
 
   onLogin() {
     // TODO: Implement your authentication logic here
     console.log('Username:', this.username);
     console.log('Password:', this.password);
+
+    if(this.username=="john.doe" && this.password=="secret"){
+      // navigate to task list
+      // this.router.navigateByUrl("/tasks/new");
+      this.router.navigate(['tasks',1])
+    }else{
+      this.isShowError = true;
+      // show error message
+    }
   }
 }

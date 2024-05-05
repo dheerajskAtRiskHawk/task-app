41db39f6cf3d01c49f4c95f33eb250114902aca8
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2024-05-06 02:20:49 +0530

    Query params.

--- src/app/task/task-list/task-list.component.ts
+++ src/app/task/task-list/task-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Task } from '../task.model';
 import * as tasksData from '../../../assets/tasks.json';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -9,11 +10,30 @@ import * as tasksData from '../../../assets/tasks.json';
   styleUrl: './task-list.component.css'
 })
 export class TaskListComponent {
+
+  constructor(private route: ActivatedRoute){
+
+  }
+
   tasks: Task[] = [];
 
   ngOnInit() {
+    // const status = this.route.snapshot.queryParamMap.get('status');
+    this.route.queryParams.subscribe((params)=>{
+      const status = params["status"];
+      console.log(status)  
+      if(status){
+        this.tasks = tasksData.tasks.filter(i => i.status == status) as Task[]
+      } else {
+        this.tasks = tasksData.tasks as Task[]
+
+      }
+     
+    })
+
+    console.log(status);
+
     console.log(tasksData.tasks)
-    this.tasks = tasksData.tasks as []; 
     console.log(tasksData)
   }
 }

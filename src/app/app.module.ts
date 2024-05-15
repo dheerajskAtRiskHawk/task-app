ac08ff5c6804bbc4fbb9dd09f7b02d88e6252d66
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2024-05-15 10:15:43 +0530

    8.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,28 +4,14 @@ import { AppComponent } from './app.component';
 import { TaskModule } from '../task/task.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { AuthModule } from './auth/auth.module';
-import { Route, RouterModule } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { TaskListComponent } from '../task/task-list/task-list.component';
 
-const routes: Route[]=[
-    {
-        path:'login', component: LoginComponent
-    },
-    {
-        path:'tasks', component: TaskListComponent
-    },
-    {
-        path:'', redirectTo:'/tasks', pathMatch: 'full'
-    }
-];
 
 
 @NgModule({
     declarations: [AppComponent],
     imports: [
-        CommonModule, TaskModule, BrowserModule, AuthModule, RouterModule.forRoot(routes)
+        CommonModule, TaskModule, BrowserModule, AuthModule
     ],
-    bootstrap:[AppComponent]
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
